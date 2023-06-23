047d7ba7bf18801b87e54cc3366e109ce32af574
LateMoon <135827734+Lmbaka@users.noreply.github.com>
2023-06-23 15:40:49 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+import psutil
+import subprocess
+import os
+import pyperclip
+import random
+print('正在寻找Minecraft开放的端口...')
+def get_open_ports():
+    all_processes = psutil.process_iter()
+    open_ports = set()  # 集合存端口
+
+    for process in all_processes:
+        try:
+            if process.name() == "javaw.exe":
+                process_connections = process.connections()
+
+                for conn in process_connections:
+                    if conn.status == 'LISTEN':
+                        open_ports.add(conn.laddr.port)  # 防重
+
+        except (psutil.Error, psutil.NoSuchProcess):
+            pass
+
+    return open_ports
+
+
+def start_frpc(minecraft_port, external_port):
+    # 清除 frpc.ini 文件内容
+    with open("frpc.ini", mode="w") as f:
+        f.write("[common]\n")
+        f.write("server_addr = gyfrp.lmbaka.top\n")
+        f.write("server_port = 54001\n")
+
+    name = "tunnel" + str(os.urandom(4).hex().upper())
+
+    # 构建 frpc.ini 配置文件内容
+    frpc_ini = f"""[tunnel_{name}]
+type = tcp
+local_ip = 127.0.0.1
+local_port = {minecraft_port}
+remote_port = {external_port}
+"""
+
+    with open("frpc.ini", mode="a") as f:
+        f.write(frpc_ini)
+
+    # 启动 frpc
+    frpc_process = subprocess.Popen(
+        ["frpc", "-c", "frpc.ini"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    target_address = f"frp.lmbaka.top:{external_port}"
+    print('__________________________________________________________')
+    print('IP:'+target_address+' 已复制到剪贴板')
+    print(f"使用 frp.lmbaka.top:{external_port} 登入房间")
+    pyperclip.copy(target_address)
+    # 输出日志信息到控制台
+    for line in iter(frpc_process.stdout.readline, b''):
+        print(line.decode('utf-8').strip())
+open_ports = get_open_ports()
+
+if len(open_ports) == 1:
+    for port in open_ports:
+        print('检测到Minecraft开放端口:'+str(port))
+        ranport = random.randint(54000, 55000)
+        start_frpc(port, ranport)
+else:
+    print("未找到Minecraft的开放端口或者有多个不同端口,你需要手动输入端口号")
+    minecraft_port = input("请输入 Minecraft 端口号：")
+    external_port = input("请输入外部端口号：")
+    ranport = random.randint(54000, 55000)
+    start_frpc(minecraft_port, ranport)
